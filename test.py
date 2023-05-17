89133c203c99fb6ea9fb22b95c54023beadbc916
ksm042900 <90131881+ksm042900@users.noreply.github.com>
2023-05-17 22:00:52 +0900

    5/17_commit
    5/17 Object detection performance improvement

--- test.py
+++ test.py
@@ -3,13 +3,17 @@ import numpy as np
 
 # webcam signal
 VideoSignal = cv2.VideoCapture(0)
+# VideoSignal.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+# VideoSignal.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
+
 # yolo weight file
 YOLO_net = cv2.dnn.readNet("yolov2-tiny.weights","yolov2-tiny.cfg")
 # print(YOLO_net)
 
-classes = []
-with open("coco.names","r") as f:
-	classes = [line.strip() for line in f.readlines()]
+classes = ["person"]
+#classes = []
+# with open("coco.names","r") as f:
+# 	classes = [line.strip() for line in f.readlines()]
 # print(classes)
 layer_names = YOLO_net.getLayerNames()
 # print(layer_names)
@@ -18,12 +22,15 @@ output_layers = [layer_names[i - 1] for i in YOLO_net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0,255,size = (len(classes),3 ))
 
 while True:
+	# read the actual frame
+	for i in range(2):
+		VideoSignal.grab()
 	# webcam Frame
 	ret, frame = VideoSignal.read()
 	h,w,c = frame.shape # height width, channel
 	
 	# yolo input
-	blob = cv2.dnn.blobFromImage(frame, 0.00392, (416,416), (0,0,0), True, crop = False)
+	blob = cv2.dnn.blobFromImage(frame, 0.00392, (320,240), (0,0,0), True, crop = False)
 	YOLO_net.setInput(blob)
 	outs = YOLO_net.forward(output_layers)
 	class_ids = []
@@ -36,7 +43,7 @@ while True:
 			class_id = np.argmax(scores)
 			confidence = scores[class_id]
 
-			if confidence > 0.1:
+			if confidence > 0.1 and classes and class_id < len(classes) and classes[class_id] == "person":
 				# Object detected
 				center_x = int(detection[0] * w)
 				center_y = int(detection[1] * h)
@@ -58,7 +65,7 @@ while True:
 			x, y, w, h = boxes[i]
 			label = str(classes[class_ids[i]])
 			score = confidences[i]
-			color = colors[i]
+			color = colors[0] # person 
 			
 			cv2.rectangle(frame, (x,y), (x+w,y+h), color,2)
 			cv2.putText(frame, label, (x,y+30),cv2.FONT_ITALIC,3, color,3)
@@ -67,4 +74,5 @@ while True:
 	
 	if cv2.waitKey(100) > 0:
 		break
+	
 	
